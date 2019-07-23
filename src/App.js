a3c43a74e9785f28b9fa55dc0a3ce1a239fa54b4
ayman-uxbert <ayman@uxbert.com>
2019-07-23 14:22:36 +0300

    add bootstrap and fix design

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import "./css/montserrat-font.css";
 import "./fonts/material-design-iconic-font/css/material-design-iconic-font.min.css";
 import UserInput from "./UserInput/UserInput";
 import UserOutput from "./UserOutput/UserOutput";
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 class App extends Component {
   render() {
@@ -13,7 +15,7 @@ class App extends Component {
           <div className="form-v10-content">
             <form className="form-detail" action="#" method="post" id="myform">
               <UserInput/>
-              <UserOutput title="businessman" fName="Ayman" lName="Albasha" position="Software Engineering" company="Uxbert" business_arena="tech"/>
+              <UserOutput title="businessman" fName="Ayman" lName="Albasha" position="Software Engineering" company="Uxbert" business_arena="tech"  cat="Trainee"/>
             </form>
           </div>
         </div>
--- src/UserOutput/UserOutput.js
+++ src/UserOutput/UserOutput.js
@@ -1,45 +1,60 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 const UserOutput = props => {
   return (
     <div className="form-right">
       <h2>Profile</h2>
-      <div className="form-row left">
-        <h5 className="data">
-          Title : <small> {props.title}</small>
-        </h5>
-      </div>
-      <div className="form-group">
-        <div className="form-row form-row-1">
-          <h5 className="data">
-            First Name : <small> {props.fName}</small>
-          </h5>
+      <div className="container p-3">
+        <div className="row ml-2">
+          <div className="col-12">
+            <h5 className="data">
+              Title : <small> {props.title}</small>
+            </h5>
+          </div>
         </div>
-        <div className="form-row form-row-2">
-          <h5 className="data">
-            Last Name : <small> {props.lName}</small>
-          </h5>
+        <div className="row ml-2 mt-5">
+          <div className="col-6">
+            <h5 className="data">
+              First Name : <small> {props.fName}</small>
+            </h5>
+          </div>
+          <div className="col-6">
+            <h5 className="data">
+              Last Name : <small> {props.lName}</small>
+            </h5>
+          </div>
         </div>
-      </div>
-      <div className="form-row left custom-element">
-        <h5 className="data">
-          Position : <small> {props.position}</small>
-        </h5>
-      </div>
-      <div className="form-group">
-        <div className="form-row form-row-1">
-          <h5 className="data">
-            Company : <small> {props.company}</small>
-          </h5>
+        <div className="row ml-2 mt-5">
+          <div className="col-12">
+            <h5 className="data">
+              Position : <small> {props.position}</small>
+            </h5>
+          </div>
         </div>
-        <div className="form-row form-row-2">
-          <h5 className="data">
-            Business Arena : <small> {props.business_arena}</small>
-          </h5>
+        <div className="row ml-2 mt-5">
+          <div className="col-12">
+            <h5 className="data">
+              Company : <small> {props.company}</small>
+            </h5>
+          </div>
         </div>
-      </div>
-      <div className="form-row-last">
-        <input type="submit" name="register" className="register" value="Update"/>
+        <div className="row ml-2 mt-5">
+          <div className="col-6">
+            <h5 className="data">
+              Business Arena : <small> {props.business_arena}</small>
+            </h5>
+          </div>
+          <div className="col-6">
+            <h5 className="data">
+              Category : <small> {props.cat}</small>
+            </h5>
+          </div>
+        </div>
+        <div className="form-row-last">
+          <input type="submit" name="register" className="register" value="Update" />
+        </div>
       </div>
     </div>
   );
